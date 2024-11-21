7b4b633f5055708ca016a8f8c70b403a1867e01a
鹿又笑 <3062840335@qq.com>
2024-11-21 21:54:17 +0800

    feat: 策略概率装配

--- big-market-app/src/test/java/com/luyou/test/ApiTest.java
+++ big-market-app/src/test/java/com/luyou/test/ApiTest.java
@@ -1,5 +1,6 @@
 package com.luyou.test;
 
+import com.luyou.domain.strategy.service.armory.IStrategyArmory;
 import com.luyou.infrastructure.persistent.dao.IStrategyAwardDao;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -17,6 +18,9 @@ public class ApiTest {
     @Resource
     private IStrategyAwardDao strategyAwardDao;
 
+    @Resource
+    private IStrategyArmory strategyArmory;
+
     @Test
     public void test() {
         log.info("测试完成");
@@ -26,5 +30,23 @@ public class ApiTest {
     public void test_infrastructure() {
         log.info("strategyAwardDao: data: {}", strategyAwardDao.queryStrategyAwardList());
     }
+
+    /**
+     * 策略ID；100001L、100002L 装配的时候创建策略表写入到 Redis Map 中
+     */
+    @Test
+    public void test_strategyArmory() {
+        boolean success = strategyArmory.assembleLotteryStrategy(100001L);
+        log.info("测试结果：{}", success);
+    }
+
+    /**
+     * 从装配的策略中随机获取奖品ID值
+     */
+    @Test
+    public void test_getAssembleRandomVal() {
+        log.info("测试结果：{} - 奖品ID值", strategyArmory.getRandomAwardId(100001L));
+    }
+
 
 }
